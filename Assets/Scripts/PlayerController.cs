f5ae9113755432dfef4f8838f5ac9ad55f2a1f93
hyojoon Lim <hyojoon42@naver.com>
2025-05-30 23:49:57 +0900

    feature : speed 추가

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 //차량 이동 제어
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 20.0f;         //차량 속도
+
     void Update()
     {
         //Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
