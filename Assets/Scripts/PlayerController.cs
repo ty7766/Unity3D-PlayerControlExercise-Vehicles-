41607ae314e645398e70158fc408304a76a052a5
hyojoon Lim <hyojoon42@naver.com>
2025-05-31 14:14:46 +0900

    fix : 횡,종이동 추가

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,11 +3,22 @@ using UnityEngine;
 //차량 이동 제어
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20.0f;         //차량 속도
+    private float speed = 20.0f;         //차량 속도
+    private float turnSpeed = 45.0f;             //좌우 이동 속도
+
+    private float horizontalInput;
+    private float forwardInput;
 
     void Update()
     {
-        //Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //Moves the car forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+
     }
 }
